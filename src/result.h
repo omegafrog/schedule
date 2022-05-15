35a6ec43e724768eef00c29a6bb6a3ae33bb4d00
omegafrog <jiwoo22110103@gmail.com>
2022-05-15 12:57:59 +0900

    process / result structure
    process:
    int type : type of input
    int process_id : pid
    int priority : priority
    int computing_time : computing time
    int startClockTime : clock time when process starts
    int endClockTime : clock time when process ends
    
    result:
    int process_id : pid
    int priority : priority
    int computing_time : computing time
    int turnaround_time : turnaround time ( endClockTime-startClocktime)

---
+++ src/result.h
@@ -0,0 +1,11 @@
+#ifndef __RESULT_H__
+
+#define __RESULT_H__
+typedef struct _RRresult
+{
+    int process_id;
+    int priority;
+    int computing_time;
+    int turnaround_time;
+} result;
+#endif
