a08e10432d0f7919e5d612a8051f2bc1c196ba55
omegafrog <jiwoo22110103@gmail.com>
2022-05-15 12:50:24 +0900

    main 함수 정의
    main.c 컴파일시 -DDEBUG옵션주면 DEBUG안 내용 수행

---
+++ src/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "scheduling.h"
+#include "process.h"
+#include "result.h"
+#include "IO.h"
+
+#define MAX_PROCESS_COUNT 100
+
+int showMenu();
+
+int main(int argc, char *argv[])
+{
+    int count = 0, selectedNum;
+    int flag = 1;
+
+    process processList[MAX_PROCESS_COUNT];
+
+    if (argc > 1)
+    {
+        count = getProcessInputFile(argv[1], processList);
+    }
+    else
+    {
+        count = getProcessInput(processList);
+    }
+
+    if (count < 0)
+    {
+        printf("데이터를 입력받는 중 오류가 발생하였습니다.");
+    }
+    else
+    {
+#ifdef DEBUG
+        for (int i = 0; i < count; ++i)
+        {
+            printf("%d %d %d %d\n", processList[i].type, processList[i].process_id, processList[i].priority, processList[i].computing_time);
+        }
+#endif
+        while (flag)
+        {
+            result *schedulingList = (result *)malloc(sizeof(result) * count);
+            switch (selectedNum = showMenu())
+            {
+            case 1:
+                // round-robin
+                round_robin(processList, count, 20, schedulingList);
+                show(processList, schedulingList, count);
+                // srtf
+                // scheduling1(processList,args, schedulingList);
+                // showOutput(schedulingList);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case -1:
+                flag = 0;
+                break;
+            }
+            free(schedulingList);
+        }
+    }
+
+    return 0;
+}
+
+int showMenu()
+{
+    int inputData;
+    printf("-----------Menu-----------\n");
+    printf(" 원하는 스케줄링 방식을 입력하시오. \n");
+    printf(" 1 : [  ]  sheduling. \n");
+    printf(" 2 : [  ]  sheduling. \n");
+    printf(" 3 : [  ]  sheduling. \n");
+    printf(" -1 : 종료 \n");
+    scanf("%d", &inputData);
+
+    return inputData;
+}
